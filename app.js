25e6351c1b71e3272cbbaddc604ecb0a699ea0a8
kasimlohar <kasimlohar2@gmail.com>
2025-01-12 17:54:23 +0530

    Intialize the server and install all packages

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/wanderlust', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB');
+}
+
+main().then(() => {
+    console.log('Connected to MongoDB');
+
+}).catch(err => {
+    console.log('Failed to connect to MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hey I am root');
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
+
+
+
