f553519c05a135a714647715a8040279a5d1f18f
kasimlohar <kasimlohar2@gmail.com>
2025-01-13 18:56:40 +0530

    Read: Show Route

--- app.js
+++ app.js
@@ -21,16 +21,25 @@ main().then(() => {
 
 app.set("view engine", "ejs");  
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Hey I am root');
 });
 
+
+//Index Route
 app.get('/listings', async (req, res) => {
     const allListings = await Listing.find({});
     res.render("./listings/index.ejs", {allListings});
 });
 
+//Show route
+app.get('/listings/:id', async (req, res) => {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", {listing});
+});
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
