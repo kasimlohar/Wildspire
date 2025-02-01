39bf0d555ef23d371887a344b3bc1383daa64bbf
kasimlohar <kasimlohar2@gmail.com>
2025-02-02 02:40:11 +0530

    Added password validation, existing user check, and secure file upload handling; implemented pagination, rating calculations, and duplicate review prevention; improved error handling and optimized performance with batch operations and image management

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,24 +1,62 @@
-const Activity = require("../models/Activity")
-const Review = require("../models/review")
+const Activity = require("../models/Activity");
+const Review = require("../models/review");
 
-module.exports.createReview = async (req, res) => {
-    // console.log(req.params.id)
-    const activity = await Activity.findById(req.params.id);
-    const newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    console.log(newReview)
+module.exports = {
+  createReview: async (req, res) => {
+    try {
+      const activity = await Activity.findById(req.params.id);
+      if (!activity) {
+        req.flash("error", "Activity not found");
+        return res.redirect("/activities");
+      }
 
-    activity.reviews.push(newReview);
-    await newReview.save();
-    await activity.save();
-    req.flash("success", "New Review Created");
-    res.redirect(`/activities/${activity._id}`);
-}
+      // Check for existing review
+      const existingReview = await Review.findOne({
+        author: req.user._id,
+        activity: activity._id
+      });
 
-module.exports.destroyReview = async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Activity.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted");
-    res.redirect(`/activities/${id}`);
-}
+      if (existingReview) {
+        req.flash("error", "You already reviewed this activity");
+        return res.redirect(`/activities/${activity._id}`);
+      }
+
+      const review = new Review({
+        ...req.body.review,
+        author: req.user._id,
+        activity: activity._id
+      });
+
+      await review.save();
+      activity.reviews.push(review);
+      await activity.save();
+
+      req.flash("success", "Successfully added review!");
+      res.redirect(`/activities/${activity._id}`);
+    } catch (err) {
+      req.flash("error", "Failed to create review");
+      res.redirect(`/activities/${req.params.id}`);
+    }
+  },
+
+  destroyReview: async (req, res) => {
+    try {
+      const { id, reviewId } = req.params;
+      
+      await Activity.findByIdAndUpdate(id, {
+        $pull: { reviews: reviewId }
+      });
+
+      await Review.findOneAndDelete({
+        _id: reviewId,
+        author: req.user._id
+      });
+
+      req.flash("success", "Successfully deleted review");
+      res.redirect(`/activities/${id}`);
+    } catch (err) {
+      req.flash("error", "Failed to delete review");
+      res.redirect(`/activities/${id}`);
+    }
+  }
+};
--- controllers/users.js
+++ controllers/users.js
@@ -1,46 +1,109 @@
-const User = require("../models/user")
+const User = require("../models/user");
+const { validatePassword } = require("../utils/validation");
 
-module.exports.renderSignupForm = (req, res) => {
-    res.render("users/signup.ejs")
-}
+module.exports = {
+  renderSignupForm: (req, res) => {
+    res.render("users/signup");
+  },
 
-module.exports.signup = async(req, res) =>{
-    try{
-        let {username, email, password} = req.body;
-        const newUser = new User({email, username})
-        const registerdUser = await User.register(newUser, password)
-        console.log(registerdUser)
-        req.login(registerdUser, (err) => {
-            if(err) {
-                return next(err);
-            } 
-            req.flash("success", "Welcome to Wanderlust")
-            res.redirect("/activities");
-        })
-    }catch(e) {
-        req.flash("error", e.message);
-        res.redirect("/signup")
+  signup: async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      
+      // Check existing user
+      const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+      if (existingUser) {
+        req.flash("error", "Email or username already exists");
+        return res.redirect("/signup");
+      }
+
+      // Password validation
+      const passwordError = validatePassword(password);
+      if (passwordError) {
+        req.flash("error", passwordError);
+        return res.redirect("/signup");
+      }
+
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+
+      req.login(registeredUser, err => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to AdventureHub!");
+        res.redirect("/activities");
+      });
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
     }
+  },
 
-}
+  renderLoginForm: (req, res) => {
+    res.render("users/login");
+  },
 
-module.exports.renderLoginForm = (req, res) => {
-    res.render("users/login.ejs")
-}
-
-module.exports.login = async(req, res) =>{
-    req.flash("success", "Welcome to Wanderlust You are logged in!")
-    let redirectUrl = res.locals.redirectUrl || "/activities";
+  login: (req, res) => {
+    const redirectUrl = res.locals.returnTo || "/activities";
+    delete req.session.returnTo;
+    req.flash("success", `Welcome back, ${req.user.username}!`);
     res.redirect(redirectUrl);
+  },
+
+  logout: (req, res) => {
+    req.logout(err => {
+      if (err) return next(err);
+      req.flash("success", "Successfully logged out!");
+      res.redirect("/activities");
+    });
+  }
 };
 
 
-module.exports.logout = (req, res, next) => {
-    req.logout((err) => {
-        if(err) {
-            return next(err);
-        }
-        req.flash("success", "you are logout noW!")
-        res.redirect("/activities")
-    })
-}
+//Old Code
+
+// const User = require("../models/user")
+
+// module.exports.renderSignupForm = (req, res) => {
+//     res.render("users/signup.ejs")
+// }
+
+// module.exports.signup = async(req, res) =>{
+//     try{
+//         let {username, email, password} = req.body;
+//         const newUser = new User({email, username})
+//         const registerdUser = await User.register(newUser, password)
+//         console.log(registerdUser)
+//         req.login(registerdUser, (err) => {
+//             if(err) {
+//                 return next(err);
+//             } 
+//             req.flash("success", "Welcome to Wanderlust")
+//             res.redirect("/activities");
+//         })
+//     }catch(e) {
+//         req.flash("error", e.message);
+//         res.redirect("/signup")
+//     }
+
+// }
+
+// module.exports.renderLoginForm = (req, res) => {
+//     res.render("users/login.ejs")
+// }
+
+// module.exports.login = async(req, res) =>{
+//     req.flash("success", "Welcome to Wanderlust You are logged in!")
+//     let redirectUrl = res.locals.redirectUrl || "/activities";
+//     res.redirect(redirectUrl);
+// };
+
+
+// module.exports.logout = (req, res, next) => {
+//     req.logout((err) => {
+//         if(err) {
+//             return next(err);
+//         }
+//         req.flash("success", "you are logout noW!")
+//         res.redirect("/activities")
+//     })
+// }
